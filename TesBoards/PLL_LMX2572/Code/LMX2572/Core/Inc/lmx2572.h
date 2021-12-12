e18541137e7cf8975325a889f6b499ae5fdbcda6
guillem.gracia <guillem.gracia@gmail.com>
2021-12-12 11:42:31 +0100

    LMX2572 library worked

--- TesBoards/PLL_LMX2572/Code/LMX2572/Core/Inc/lmx2572.h
+++ TesBoards/PLL_LMX2572/Code/LMX2572/Core/Inc/lmx2572.h
@@ -33,35 +33,35 @@ extern "C" {
 static uint32_t R[126];		// Registers Bank
 
 typedef struct PLL{
-	float 		frequency;		// Target frequency in MHz
-	float 		Fvco;			// Computed frequency of the VCO
-	float 		fpd;			// Computed frequency of the PD
+	float frequency;		// Target frequency in MHz
+	float Fvco;				// Computed frequency of the VCO
+	float fpd;				// Computed frequency of the PD
 
-	float 		fosc;			// OSCin frequency in MHz
-	uint8_t 	doubler;		// OSC doubler
-	uint8_t 	r_pre;
-	uint8_t 	r;
-	uint8_t 	mult;
-	uint16_t 	n;
-	uint32_t 	num;
-	uint32_t 	den;
+	float fosc;				// OSCin frequency in MHz
+	uint8_t doubler;			// OSC doubler
+	uint8_t r_pre;
+	uint8_t r;
+	uint8_t mult;
+	uint16_t n;
+	uint32_t num;
+	uint32_t den;
 
-	uint16_t 	CPG;
-	uint8_t 	VCO;
-	uint8_t 	VCO_force;
-	uint8_t 	MASH_order;
-	uint8_t 	PFD_DLY_SEL;
+	uint16_t CPG;
+	uint8_t VCO;
+	uint8_t VCO_force;
+	uint8_t MASH_order;
+	uint8_t PFD_DLY_SEL;
 
-	uint8_t 	out_mux_a;		// CHDIV = 0, VCO = 1, HI_Z = 3
-	uint8_t 	out_mux_b;		// VCO = 1, CHDIV = 2, HI_Z = 3
-	uint8_t 	chdiv;
-	uint8_t		out_pwr_a;		// RFoutA Power
-	uint8_t 	out_pwr_b;		// RFoutB Power
-	uint8_t 	out_pd_a;		// On = 0, Off = 1
-	uint8_t 	out_pd_b;		// On = 0, Off = 1
+	uint8_t out_mux_a;				// CHDIV = 0, VCO = 1, HI_Z = 3
+	uint8_t out_mux_b;				// VCO = 1, CHDIV = 2, HI_Z = 3
+	uint8_t chdiv;
+	uint8_t out_pwr_a;				// RFoutA Power
+	uint8_t out_pwr_b;				// RFoutB Power
+	uint8_t out_pd_a;				// On = 0, Off = 1
+	uint8_t out_pd_b;				// On = 0, Off = 1
 } PLL;
 
-// **** FUNCTION PROTOTYPES ***********************************
+// **** FUNCTION PROTOTYPES********************************************
 PLL 		LMX2572_det_param(struct PLL pll);
 PLL			LMX2572_defaultConfig(struct PLL pll);
 PLL			LMX2572_loadPLL(struct PLL pll);
@@ -74,25 +74,23 @@ void 		LMX2572_reset(SPI_HandleTypeDef *hspi);
 void 		LMX2572_set_cpg(struct PLL pll);
 void	 	LMX2572_set_fpd(struct PLL pll);
 void 		LMX2572_set_frequency(struct PLL pll);
-void 		LMX2752_VCO_assist(struct PLL pll);
-void 		LMX2572_VCO_calibrate(struct PLL pll);
+void 		LMX2752_vco_assist(struct PLL pll);
+void 		LMX2572_calibrate_VCO(struct PLL pll);
 void 		LMX2572_set_CHDIV(struct PLL pll);
 void 		LMX2572_set_MASH(struct PLL pll);
 
-void 		LMX2572_RFoutA_On(struct PLL pll);
-void 		LMX2572_RFoutA_Off(struct PLL pll);
-void 		LMX2572_RFoutB_On(struct PLL pll);
-void 		LMX2572_RFoutB_Off(struct PLL pll);
-void 		LMX2572_RFoutA_mux(struct PLL pll);
-void 		LMX2572_RFoutB_mux(struct PLL pll);
-void 		LMX2572_RFoutA_pwr(struct PLL pll);
-void 		LMX2572_RFoutB_pwr(struct PLL pll);
+void 		LMX2572_on_PLL(SPI_HandleTypeDef *hspi);
+void 		LMX2572_off_PLL(SPI_HandleTypeDef *hspi);
+void 		LMX2572_switchOn_RFoutA(struct PLL pll);
+void 		LMX2572_switchOff_RFoutA(struct PLL pll);
+void 		LMX2572_switchOn_RFoutB(struct PLL pll);
+void 		LMX2572_switchOff_RFoutB(struct PLL pll);
+void 		LMX2572_mux_RFoutA(struct PLL pll);
+void 		LMX2572_mux_RFoutB(struct PLL pll);
+void 		LMX2572_pwr_RFoutA(struct PLL pll);
+void 		LMX2572_pwr_RFoutB(struct PLL pll);
 
-void 		LMX2572_on(SPI_HandleTypeDef *hspi);
-void 		LMX2572_off(SPI_HandleTypeDef *hspi);
 PLL 		LMX2572_init(struct PLL pll, SPI_HandleTypeDef *hspi);
-PLL 		LMX2572_frequency(struct PLL pll, SPI_HandleTypeDef *hspi, float frequency);
-PLL 		LMX2572_powerA(struct PLL pll, SPI_HandleTypeDef *hspi, uint8_t power);
 
 #ifdef __cplusplus
 }
